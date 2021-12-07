742b9e98e35930a37f403c2771fbfc51451ee0fb
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-12-07 04:16:19 +0100

    set up project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Converter />
+</template>
+
+<script>
+import { useQueryProvider } from 'vue-query';
+import Converter from './components/Converter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Converter,
+  },
+  setup() {
+    useQueryProvider();
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
